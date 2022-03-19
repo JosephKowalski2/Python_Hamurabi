6077c961c2f1173cdca285b0a72a774dbaca2160
Joseph Kowalski <jkjr25@gmail.com>
2022-03-19 14:56:35 -0400

    Added initialized variables

--- hamurabi.py
+++ hamurabi.py
@@ -1,1 +1,20 @@
-class Hamurabi:
+def playgame:
+        years = 1
+        population = 100
+        bushels = 2800
+        acres = 1000
+        price = 19
+        acres_bought = 0
+        acres_sold = 0
+        number_of_plague = 0
+        bushels_fed_to_people = 0
+        harvest = 0
+        deaths = 0
+        immigrants = 0
+        grain_eaten = 0
+        acres_planted = 0
+        bushels_used_as_seed = 0
+        total_deaths = 0
+        total_immigration = 0
+        total_grain_eaten_by_rats = 0
+
